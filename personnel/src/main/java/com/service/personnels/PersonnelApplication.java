f94d0eb2ea6a31738b3182223314ef1ca560f1b8
najla daoud <najla.daoud@esprit.tn>
2021-10-25 15:18:33 +0100

    update class main du microservice personnel

--- personnel/src/main/java/com/service/personnels/PersonnelApplication.java
+++ personnel/src/main/java/com/service/personnels/PersonnelApplication.java
@@ -1,16 +1,11 @@
 package com.service.personnels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 
 @SpringBootApplication
-@ComponentScan({"com.service.personnels.controller", "com.service.personnels.Service"})
-@EntityScan("com.service.personnels.entities")
-@EnableJpaRepositories("com.service.personnels.Repositorie")
 @EnableEurekaClient
 public class PersonnelApplication {
 
