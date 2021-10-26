4bcea55036dd1c233db3ba4dc956bfb403353076
RaedJarboui <raedjarboui1998@gmail.com>
2021-10-26 23:15:05 +0200

    aero+personnel microservices

---
+++ personnel/src/test/java/com/service/personnels/PersonnelApplicationTests.java
@@ -0,0 +1,15 @@
+package com.service.personnels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+	
+	
+}
