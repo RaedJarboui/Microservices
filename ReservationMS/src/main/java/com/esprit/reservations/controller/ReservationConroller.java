fd23efb7b1c26ffdfec0d93365e0a9837d527902
Imen EL HAKIM <imen.elhakim@esprit.tn>
2021-10-28 01:18:33 +0200

    template integration

--- ReservationMS/src/main/java/com/esprit/reservations/controller/ReservationConroller.java
+++ ReservationMS/src/main/java/com/esprit/reservations/controller/ReservationConroller.java
@@ -1,9 +1,13 @@
 package com.esprit.reservations.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,12 +20,22 @@ import com.esprit.reservations.entity.Reservation;
 import com.esprit.reservations.service.ReservationRestAPI;
 
 @RestController
+@CrossOrigin("http://localhost:4200/")
 @RequestMapping("api/reservations")
 public class ReservationConroller {
 	
 	@Autowired
 	private ReservationRestAPI reservationRestAPI;
 
+	
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Reservation>> getAeros(@RequestBody Reservation personnel) {
+		return new ResponseEntity<>(reservationRestAPI.getAllReservation(), HttpStatus.OK);
+	}
+
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation){
